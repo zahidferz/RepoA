47e59965c2c3a1e7cfb85bbbe8e331dfacbb178b
zahid.ferz@gmail.com <z@zmac.local>
2020-06-22 23:08:39 -0500

    Hooks 1.0.0

--- Hook.ps1
+++ Hook.ps1
@@ -1,9 +1,17 @@
-$id=$env:id
+$buildid=$env:buildid
 $token=$env:token
 $stage=$env:stage
-$sysid=$env:sysid
+$projectid=$env:projectid
+$versionid=$env:versionid
+$webappName=$env:webappName
+
 $header = @{authorization = "Bearer $token"}
-$Body = Get-Content ./slack.json | ConvertFrom-Json
+$Body = Get-Content ./SuccededDeploy.json | ConvertFrom-Json
 $Body.blocks.text.text = "<https://dev.azure.com/gestionix-boa/$sysid/_build/results?buildId=$id&branchName=master| $stage>"
+
+$Section1=($Body.attachments.blocks | where-object block_id -eq section1).text.text -creplace "projectid", $projectid -creplace "buildid", $buildid -creplace "enviroment", $stage
+($Body.attachments.blocks | where-object block_id -eq section1).text.text= $Section1
+$Section2=($Body.attachments.blocks | where-object block_id -eq section2).text.text -creplace "projectid", $projectid -creplace "buildid", $buildid -creplace "ID", $versionid -creplace "Webapp", $webappName
+($Body.attachments.blocks | where-object block_id -eq section2).text.text= $Section2
 $Body = $Body | ConvertTo-Json -Depth 5
 Invoke-RestMethod -Uri "https://eleva-inc.slack.com/api/chat.postMessage"  -Method Post -ContentType "application/json" -Headers $header -Body $Body
