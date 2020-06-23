cdaf4675cf3b94fa5022797113f3e26391a74758
ZahidFernandez <zahid.ferz@gmail.com>
2020-06-22 21:02:46 -0500

    Update Hook.ps1

--- Hook.ps1
+++ Hook.ps1
@@ -1,7 +1,8 @@
 $id=$env:id
 $token=$env:token
+$stage=$env:stage
 $header = @{authorization = "Bearer $token"}
 $Body = Get-Content ./slack.json | ConvertFrom-Json
-$Body.blocks.text.text = "<https://dev.azure.com/gestionix-boa/gx-sandbox-pipeline/_build/latest?definitionId=$id&branchName=master| Stage>"
+$Body.blocks.text.text = "<https://dev.azure.com/gestionix-boa/gx-sandbox-pipeline/_build/latest?definitionId=$id&branchName=master| $stage>"
 $Body = $Body | ConvertTo-Json -Depth 5
 Invoke-RestMethod -Uri "https://eleva-inc.slack.com/api/chat.postMessage"  -Method Post -ContentType "application/json" -Headers $header -Body $Body
